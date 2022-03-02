1c896eb8dcb16804bb12058c94e22d7637e42056
brookezhang <brookeqingzhang@gmail.com>
2022-03-01 20:37:13 -0800

    FINALLY DONE

--- flaskapi.py
+++ flaskapi.py
@@ -3,12 +3,17 @@ from AzureReceiptRec import AzureReceipt
 import json
 app = Flask(__name__)
 
-@app.route('/get_items', methods=['POST'])
-def get_items():
+@app.route('/')
+def hello_world():
+    return json.dumps("Hello World!")
+
+@app.route('/items', methods=['GET','POST'])
+def items():
     reciept = request.json
     imgStr = reciept["base64"]
     r = AzureReceipt()
     t = r.get(imgStr)
+    print(t)
     if t == 'Error':
         return json.dumps("Error"), 400
     else:
